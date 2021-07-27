3f3620e59b2b4b19f84dffd335262d1d1d6e852c
ShimonIslam <shimon.ocean@gmail.com>
2021-07-26 20:26:10 -0700

    Tailwind imports + 1st button

---
+++ components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
+function Header() {
+    return (
+        <div className='flex items-center'>
+            <Button
+                color="blue"
+                buttonType="outline"
+                rounded={true}
+                iconOnly={true}
+                ripple="dark"
+                className="md:inline-flex h-20 w-20 border-0"
+            
+            >
+                <Icon name="menu" size="3xl"/>
+            </Button>
+        </div>
+    )
+}
+
+export default Header
